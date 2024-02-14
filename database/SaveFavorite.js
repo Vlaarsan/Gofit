5070d16f216edc99fa8028fe3b844bb5597d1820
Vlaarsan <priamdavidpro@gmail.com>
2024-02-14 15:18:11 +0100

    Ajout des fav en bdd

--- database/SaveFavorite.js
+++ database/SaveFavorite.js
@@ -1,19 +1,29 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { auth, firestore } from "../firebase/config";
-import { collection, doc, updateDoc, setDoc} from "firebase/firestore";
+import { collection, doc, updateDoc, setDoc, getDoc} from "firebase/firestore";
 
 const SaveFavorite = async (user, newFavorite) => {
   try {
     // Référence au document de l'utilisateur spécifique
-    const userDocRef = doc(firestore, "users", user.uid);
+    const userDocRef = doc(firestore, "favorite", user.uid);
 
-    // Utilisation de updateDoc pour ajouter ou mettre à jour le champ favorite dans le document utilisateur
-    await setDoc(userDocRef, {
-      favorite: newFavorite,
-    });
+    // Récupère le document utilisateur
+    const userDocSnapshot = await getDoc(userDocRef);
 
-    console.log("Champ 'favorite' ajouté avec succès !");
+    if (userDocSnapshot.exists()) {
+      // Si le document existe, utilise updateDoc pour mettre à jour le champ favorite
+      await updateDoc(userDocRef, {
+        favorite: newFavorite,
+      });
+      console.log("Champ 'favorite' mis à jour avec succès !");
+    } else {
+      // Si le document n'existe pas, utilise setDoc pour créer le document avec le champ favorite
+      await setDoc(userDocRef, {
+        favorite: newFavorite,
+      });
+      console.log("Champ 'favorite' ajouté avec succès !");
+    }
   } catch (error) {
     console.error("Erreur lors de l'enregistrement des données : ", error);
   }
--- screens/InsightScreen.jsx
+++ screens/InsightScreen.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useLikedExercisesContext } from '../context/LikedExercicesContext';
 import { useUserContext } from '../context/UserContext';
@@ -6,13 +6,19 @@ import ExoCard from '../components/ExoCard';
 import SaveFavorite from '../database/SaveFavorite';
 
 
+
 const InsightScreen = () => {
   const { exercises } = useLikedExercisesContext();
   const { user, setUserContext } = useUserContext();
+  const previousExercises = useRef([]);
 
   useEffect(() => {
-    SaveFavorite(user.uid, exercises);
-  }, [exercises]);
+    // Vérifie si le tableau n'était pas vide auparavant ou s'il contient des éléments actuellement
+    if (previousExercises.current.length === 0 || exercises.length > 0) {
+      SaveFavorite(user, exercises);
+    }
+  }, [user, exercises]);
+  
 
   return (
     <View style={styles.container}>
