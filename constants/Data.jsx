8f5791c5f45bffd11669ab73f75d2cc7974bd113
Vlaarsan <priamdavidpro@gmail.com>
2024-02-14 14:20:09 +0100

    .

--- constants/Data.jsx
+++ constants/Data.jsx
@@ -1,27 +1,27 @@
 export const exoData = [
   {
-    id: "1",
+    id: "0",
     name: "Pompes",
     url: "https://images.pexels.com/photos/1552252/pexels-photo-1552252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     category: "Pecs",
     exempleImg : "https://www.docteur-fitness.com/wp-content/uploads/2020/10/pompe-musculation.gif",
   },
   {
-    id: "2",
+    id: "1",
     name: "Dos",
     url: "https://images.pexels.com/photos/3775164/pexels-photo-3775164.jpeg",
     category: "Dos",
     exempleImg : "https://www.docteur-fitness.com/wp-content/uploads/2020/10/pompe-musculation.gif"
   },
   {
-    id: "3",
+    id: "2",
     name: "Squat",
     url: "https://images.pexels.com/photos/1552252/pexels-photo-1552252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     category: "Fessier",
     exempleImg : "https://www.docteur-fitness.com/wp-content/uploads/2020/10/pompe-musculation.gif"
   },
   {
-    id: "4",
+    id: "3",
     name: "Curl barre",
     url: "https://images.pexels.com/photos/1552252/pexels-photo-1552252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     category: "Biceps",
